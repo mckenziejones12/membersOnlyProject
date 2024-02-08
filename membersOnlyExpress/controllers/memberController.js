7a5b0af5cf35e8e7f845c8cac700735fa19b33fc
McKenzie Jones <mckenzbeck12@gmail.com>
2024-02-08 16:33:11 -0700

    Add password hashing

--- membersOnlyExpress/controllers/memberController.js
+++ membersOnlyExpress/controllers/memberController.js
@@ -1,27 +1,40 @@
 const asyncHandler = require("express-async-handler");
 const Message = require("../models/message");
 const Member = require("../models/member");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.SECRET_TOKEN;
+const bcrypt = require("bcryptjs");
 
 // Handle login form
 exports.member_login_post = asyncHandler(async (req, res, next) => {
   console.log("login route is working");
+  console.log(jwtSecret);
   const { username, password } = req.body;
+  // Check if username and password is provided
   if (!username || !password) {
     return res.status(400).json({
       message: "Username or Password not present",
     });
   }
   try {
-    const member = await Member.findOne({ username, password });
+    const member = await Member.findOne({ username });
     if (!member) {
       res.status(401).json({
         message: "Login not successful",
         error: "User not found",
       });
     } else {
-      res.status(200).json({
-        message: "Login successful",
-        member,
+      // compare given password with hashed password
+      bcrypt.compare(password, member.password).then((result) => {
+        console.log("the result: ", result);
+        result
+          ? res.status(200).json({
+              message: "Login successful",
+              member,
+            })
+          : res.status(400).json({
+              message: "Login not successful",
+            });
       });
     }
   } catch (error) {
--- membersOnlyExpress/models/member.js
+++ membersOnlyExpress/models/member.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MemberSchema = new Schema({
-  username: { type: String, required: true, minLength: 3, maxLength: 15 },
-  password: { type: String, required: true, minLength: 8, maxLength: 15 },
+  username: { type: String, required: true, minLength: 3 },
+  password: { type: String, required: true, minLength: 8 },
 });
 
 module.exports = mongoose.model("Member", MemberSchema);
